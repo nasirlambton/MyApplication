cc4421932507f76391cf54ada98ae79687edcf0b
nasirlambton <nasirlambton@gmail.com>
2026-04-08 23:02:05 -0600

    Added another comment

--- app/src/main/java/com/example/myapplication/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication/app/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+// This is main activity class
 public class MainActivity extends ActionBarActivity {
 
     @Override
